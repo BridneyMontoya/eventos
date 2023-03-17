7bf84cf149cb141961e7a412a09e9d6ad46e4351
Bridney <est.bcastrom@uml.edu.ni>
2023-03-17 17:21:04 -0600

    Diseño: Integracion de Flowbite y barra de navegación inferior

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,3 +6,17 @@ module.exports = {
   },
   plugins: [],
 }
+
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}","*.html","*.css",
+  "./node_modules/flowbite/**/*.js" // Esta linea se agrega para que tailwind cargue la paquete Flowbite
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin') //Esta linea se agrega para cargar la declaración del plugin Flowbite
+  ],
+}
